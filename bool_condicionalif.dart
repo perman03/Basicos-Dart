3f21c27637513e9aa72685440520d82caf3eac49
Franco Perez <frankperman03@gmail.com>
2022-08-08 08:33:33 -0500

    booleanos y condicion if

---
+++ bool_condicionalif.dart
@@ -0,0 +1,16 @@
+void main(){
+  
+//   Valores booleanos
+  
+  bool? isActive = null;  //Para que un booleano tengo un valor nulo, se debe colocar el signo de interrogacion
+  
+//   Condicional if
+  if ( isActive == null ){    //si isActive es nulo...
+    print( 'Es nulo' );       // ejecuta esto
+  } else {                    // si no...
+    print( 'No es nulo' );    // ejecuta esto
+  }
+  
+  
+  
+}
